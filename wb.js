66329b9d83dc2cbe254a26901bdde0c8c8b64be2
wbjs <wbjs@outlook.com>
2015-01-15 13:53:42 +0800

    prototype

--- wb.js
+++ wb.js
@@ -5,178 +5,168 @@
 var Wb=function(s){
     this.objs=document.querySelectorAll(s);
 };
-
-/* display */
-Wb.prototype.hide=function(){
-    for(i=0;i<this.objs.length;i++){
-        this.objs[i].style.display="none";
-    }
-    return this;
-}
-Wb.prototype.show=function(){
-    for(i=0;i<this.objs.length;i++){
-        this.objs[i].style.display="block";
-    }
-    return this;
-}
-/* innerHTML */
-Wb.prototype.text=function(t){
-
-    if(t==undefined){
-        var arr=new Array();
+Wb.prototype={
+    /* display */
+    hide:function(){
         for(i=0;i<this.objs.length;i++){
-            var value=this.objs[i].innerHTML;
-            arr.push(value);
-        }
-        if(arr.length>1){
-            return arr;
-        }else{
-            return arr[0];
-        }
-    }else{
-        for(i=0;i<this.objs.length;i++){
-            this.objs[i].innerHTML=t;
+            this.objs[i].style.display="none";
         }
         return this;
-    }
-}
-
-/* random Array */
-Wb.prototype.randomArray=function(){
-    console.log(this.objs);
-    this.objs=this.objs.sort(function(){
-        return Math.random() > 0.5 ? -1 : 1;
-    });
-    return this;
-}
-
-/* onload */
-Wb.prototype.ready=function(x){
-    window.onload=x;
-}
-
-/* pc mouse over and out */
-Wb.prototype.hover=function(x){
-    for(i=0;i<this.objs.length;i++){
-        this.objs[i].onmouseover=x;
-    }
-    return this;
-}
-Wb.prototype.out=function(x){
-    for(i=0;i<this.objs.length;i++){
-        this.objs[i].onmouseout=x;
-    }
-    return this;
-}
-/* onclick */
-Wb.prototype.click=function(x){
-    for(i=0;i<this.objs.length;i++){
-        if(/iphone|nokia|sony|ericsson|mot|samsung|sgh|lg|philips|panasonic|alcatel|lenovo|cldc|midp|wap|android|iPod/i.test(navigator.userAgent.toLowerCase())){
-            this.objs[i].addEventListener("touchstart",x,false);
-        }else{
-            this.objs[i].onclick=x;
-        }
-    }
-    return this;
-}
-
-Wb.prototype.clearClass=function(){
-    for(i=0;i<this.objs.length;i++){
-        this.objs[i].removeAttribute("class");
-    }
-    return this;
-}
-Wb.prototype.clearAttr=function(a){
-    for(i=0;i<this.objs.length;i++){
-        this.objs[i].removeAttribute(a);
-    }
-    return this;
-}
-Wb.prototype.attr=function(a,b){
-    /* 2014 12 30 */
-    if(b==undefined){
-        var arr=new Array();
+    },
+    show:function(){
         for(i=0;i<this.objs.length;i++){
-            var value=this.objs[i].getAttribute(a);
-            arr.push(value);
-        }
-        if(arr.length>1){
-            return arr;
-        }else{
-            return arr[0];
-        }
-    }else{
-        for(i=0;i<this.objs.length;i++){
-            this.objs[i].setAttribute(a,b);
-            console.log(b)
+            this.objs[i].style.display="block";
         }
         return this;
-    }
-}
-
-/* setAttribute：value from myself */
-Wb.prototype.attrSelf=function(a,b){
-    for(i=0;i<this.objs.length;i++){
-        eval("this.objs[i].setAttribute(a,this.objs[i]."+b+")");
-    }
-    return this;
-}
-/* from 1 to end+1  */
-Wb.prototype.initIndex=function(){
-    for(i=0;i<this.objs.length;i++){
-        this.objs[i].setAttribute("data-index",(i+1));
-    }
-    return this;
-}
-Wb.prototype.count=function(a){
-    a["count"]=this.objs.length;
-    return this;
-}
-/* style 2014 12 30 */
-Wb.prototype.style=function(a,b){
+    },
+    /* innerHTML */
+    text:function(t){
+        if(t==undefined){
+            var arr=new Array();
+            for(i=0;i<this.objs.length;i++){
+                var value=this.objs[i].innerHTML;
+                arr.push(value);
+            }
+            if(arr.length>1){
+                return arr;
+            }else{
+                return arr[0];
+            }
+        }else{
+            for(i=0;i<this.objs.length;i++){
+                this.objs[i].innerHTML=t;
+            }
+            return this;
+        }
+    },
+    /* random Array */
+    randomArray:function(){
+        console.log(this.objs);
+        this.objs=this.objs.sort(function(){
+            return Math.random() > 0.5 ? -1 : 1;
+        });
+        return this;
+    },
+    /* onload */
+    ready:function(x){
+        window.onload=x;
+    },/* pc mouse over and out */
+    hover:function(x){
+        for(i=0;i<this.objs.length;i++){
+            this.objs[i].onmouseover=x;
+        }
+        return this;
+    },
+    out:function(x){
+        for(i=0;i<this.objs.length;i++){
+            this.objs[i].onmouseout=x;
+        }
+        return this;
+    },
+    /* onclick */
+    click:function(x){
+        for(i=0;i<this.objs.length;i++){
+            if(/iphone|nokia|sony|ericsson|mot|samsung|sgh|lg|philips|panasonic|alcatel|lenovo|cldc|midp|wap|android|iPod/i.test(navigator.userAgent.toLowerCase())){
+                this.objs[i].addEventListener("touchstart",x,false);
+            }else{
+                this.objs[i].onclick=x;
+            }
+        }
+        return this;
+    },
+    clearClass:function(){
+        for(i=0;i<this.objs.length;i++){
+            this.objs[i].removeAttribute("class");
+        }
+        return this;
+    },
+    clearAttr:function(a){
+        for(i=0;i<this.objs.length;i++){
+            this.objs[i].removeAttribute(a);
+        }
+        return this;
+    },
+    attr:function(a,b){
     /* 2014 12 30 */
-    if(b==undefined){
+        if(b==undefined){
+            var arr=new Array();
+            for(i=0;i<this.objs.length;i++){
+                var value=this.objs[i].getAttribute(a);
+                arr.push(value);
+            }
+            if(arr.length>1){
+                return arr;
+            }else{
+                return arr[0];
+            }
+        }else{
+            for(i=0;i<this.objs.length;i++){
+                this.objs[i].setAttribute(a,b);
+                console.log(b)
+            }
+            return this;
+        }
+    },
+    /* setAttribute：value from myself */
+    attrSelf:function(a,b){
+        for(i=0;i<this.objs.length;i++){
+            eval("this.objs[i].setAttribute(a,this.objs[i]."+b+")");
+        }
+        return this;
+    },
+    /* from 1 to end+1  */
+    initIndex:function(){
+        for(i=0;i<this.objs.length;i++){
+            this.objs[i].setAttribute("data-index",(i+1));
+        }
+        return this;
+    },
+    count:function(a){
+        a["count"]=this.objs.length;
+        return this;
+    },
+    /* style 2014 12 30 */
+    style:function(a,b){
+        /* 2014 12 30 */
+        if(b==undefined){
+            var arr=new Array();
+            for(i=0;i<this.objs.length;i++){
+                var cmd="var value=this.objs[i].style."+a;
+                eval(cmd);
+                arr.push(value);
+            }
+            if(arr.length>1){
+                return arr;
+            }else{
+                return arr[0];
+            }
+        }else{
+            for(i=0;i<this.objs.length;i++){
+                var cmd="this.objs[i].style."+a+"='"+b+"'";
+                //console.log(cmd)
+                eval(cmd);
+            }
+            return this;
+        }
+    },
+    selectIndex:function(i){
+        i=i-1;
+        var arr=new Array();
+        arr[0]=this.objs[i];
+        this.objs=arr;
+        return this;
+    },select:function(obj){
         var arr=new Array();
         for(i=0;i<this.objs.length;i++){
-            var cmd="var value=this.objs[i].style."+a;
-            eval(cmd);
-            arr.push(value);
+            if(this.objs[i]==obj){
+                arr[0]=this.objs[i];
+            }
         }
-        if(arr.length>1){
-            return arr;
-        }else{
-            return arr[0];
-        }
-    }else{
-        for(i=0;i<this.objs.length;i++){
-            var cmd="this.objs[i].style."+a+"='"+b+"'";
-            //console.log(cmd)
-            eval(cmd);
-        }
+        this.objs=arr;
         return this;
     }
 }
 Wb.prototype.css=Wb.prototype.style;/* like jq */
-/* select like  linQ */
-/* selct from 1 */
-Wb.prototype.selectIndex=function(i){
-    i=i-1;
-    var arr=new Array();
-    arr[0]=this.objs[i];
-    this.objs=arr;
-    return this;
-}
-Wb.prototype.select=function(obj){
-    var arr=new Array();
-    for(i=0;i<this.objs.length;i++){
-        if(this.objs[i]==obj){
-            arr[0]=this.objs[i];
-        }
-    }
-    this.objs=arr;
-    return this;
-}
-
 
 /*
  * plus
